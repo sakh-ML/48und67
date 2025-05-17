2987734ca1436e7813944021c4c2a5f4e8b15909
yazanmawassi <yazanmawass135@gmail.com>
2025-05-17 22:34:40 +0200

    i added anew java function

--- hello.java
+++ hello.java
@@ -1,1 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int zahl = 4;
+        if (istGerade(zahl)) {
+            System.out.println(zahl + " ist gerade.");
+        } else {
+            System.out.println(zahl + " ist ungerade.");
+        }
+    }
+
+    public static boolean istGerade(int n) {
+        return n % 2 == 0;
+    }
+}
 
