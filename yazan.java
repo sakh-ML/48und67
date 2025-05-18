8fc43117aeec3660ca0bcb6b3387d1fe487a6a60
yazanmawassi <yazanmawass135@gmail.com>
2025-05-18 10:27:53 +0200

    i added a java program

---
+++ yazan.java
@@ -0,0 +1,17 @@
+public class HalloJava {
+    public static void main(String[] args) {
+        // Begrüßung
+        System.out.println("Hallo, willkommen in Java!");
+
+        // Zwei Zahlen definieren
+        int zahl1 = 7;
+        int zahl2 = 5;
+
+        // Addition
+        int summe = zahl1 + zahl2;
+
+        // Ergebnis ausgeben
+        System.out.println("Die Summe von " + zahl1 + " und " + zahl2 + " ist: " + summe);
+    }
+}
+
